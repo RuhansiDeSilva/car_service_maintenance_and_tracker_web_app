77bcdfea584f32eac32ec7595d1d6e788dbbd08a
Sadeesha LK <99633523+SadeeshaLK@users.noreply.github.com>
2025-05-20 14:45:24 +0530

    Update DeleteFeedbackServlet.java

--- src/main/java/lk/sliit/carservicemanagementgp99/projectname/DeleteFeedbackServlet.java
+++ src/main/java/lk/sliit/carservicemanagementgp99/projectname/DeleteFeedbackServlet.java
@@ -39,7 +39,7 @@ public class DeleteFeedbackServlet extends HttpServlet {
             }
         }
 
-        // Re
+        // R
         Files.delete(inputFile.toPath());
         Files.move(tempFile.toPath(), inputFile.toPath());
 
